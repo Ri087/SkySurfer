40b6fae310b01c43e902f587a0c44cfef6a6ad28
Bvernet <benjamin.vernet@ynov.com>
2023-02-06 10:53:49 +0100

    Opening black window

--- SkySurfer/Program.cs
+++ SkySurfer/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace SkyRunner
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            // Create the window
+            RenderWindow window = new RenderWindow(new VideoMode(800, 500), "My game");
+
+            // Run the game loop
+            while (window.IsOpen)
+            {
+                // Handle events
+                window.DispatchEvents();
+
+                // Update the game
+
+                // Draw the game
+                window.Clear();
+
+                // ... draw your game here ...
+                window.Display();
+            }
+        }
+    }
+}
