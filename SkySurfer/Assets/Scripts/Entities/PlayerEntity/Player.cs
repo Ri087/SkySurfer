75c68e71647b2490f234bac8855bc998874e1be4
Bvernet <benjamin.vernet@ynov.com>
2023-02-07 12:18:46 +0100

    [Create] Creation of the entity player

---
+++ SkySurfer/Assets/Scripts/Entities/PlayerEntity/Player.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySurfer.Assets.Scripts.Entities.PlayerEntity
+{
+    class Player
+    {
+        private float _powerUpTime;
+        private int _hp;
+        private int _jump;
+        private float _attackSpeed;
+        private float _invulnerableTime;
+        private int _gravity;
+
+        public float GetPowerUpTime()
+        {
+            return _powerUpTime;
+        }
+        public void SetPowerUpTime(float time)
+        {
+            _powerUpTime = time;
+        }
+
+        public float GetHp()
+        {
+            return _hp;
+        }
+
+        public void SetHp(int hp)
+        {
+            _hp = hp;
+        }
+        public int GetJump()
+        {
+            return _jump;
+        }
+
+        public void SetJump(int jump)
+        {
+            _jump = jump;
+        }
+
+        public float GetAttackSpeed()
+        {
+            return _attackSpeed;
+        }
+        public void SetAttackSpeed(float attackSpeed)
+        {
+            _attackSpeed = attackSpeed; 
+        }
+
+        public float GetInvulnerableTime()
+        {
+            return _invulnerableTime;
+        }
+
+        public void SetInvulerableTime(float time)
+        {
+            _invulnerableTime = time;
+        }
+
+        public int GetGravity()
+        {
+            return _gravity;
+        }
+
+        public void SetGravity(int gravity)
+        {
+            _gravity = gravity;
+        }
+    }
+}
---
+++ SkySurfer/Assets/Scripts/Entities/PlayerStateManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkySurfer.Assets.Scripts.Entities.PlayerEntity;
+
+namespace SkySurfer.Assets.Scripts.Entities
+{
+    class PlayerStateManager
+    {
+        private Player _player = new();
+        private ClassicState _classicState = new();
+        private HeavyState _heavyState = new();
+        private FallState _fallState = new();
+        private StickyState _stickyState = new();
+        private Stack<PlayerBaseState> _states = new();
+        public PlayerStateManager()
+        {
+            _states.Push(_classicState);
+        }
+
+        public Player GetPlayer()
+        {
+            return _player;
+        }
+        public ClassicState GetClassicState()
+        {
+            return _classicState;
+        }
+
+        public HeavyState GetHeavyState()
+        {
+            return _heavyState;
+        }
+
+        public FallState GetFallState()
+        {
+            return _fallState;
+        }
+
+        public StickyState GetStickyState()
+        {
+            return _stickyState;
+        }
+
+        public Stack<PlayerBaseState> GetStates()
+        {
+            return _states;
+        }
+    }
+}
