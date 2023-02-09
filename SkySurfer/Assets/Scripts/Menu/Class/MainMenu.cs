3cd7edd6ab44f21b54d0f73d485f1cf0e1f55a72
Bvernet <benjamin.vernet@ynov.com>
2023-02-09 11:06:02 +0100

    [menu] optimisation du menu

--- SkySurfer/Assets/Scripts/Menu/Class/MainMenu.cs
+++ SkySurfer/Assets/Scripts/Menu/Class/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SFML.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,45 @@ namespace SkySurfer.Assets.Scripts.Menu.Class
 {
     class MainMenu
     {
-        public string text;
-        public uint size;
+        public string text = "Placeholder";
+        public uint size = 55;
         public float x;
         public float y;
+        public Color color = Color.White;
+        public MainMenu(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
+        public MainMenu(string text, float x, float y)
+        {
+            this.text = text;
+            this.x = x;
+            this.y = y;
+        }
+
+        public MainMenu(string text, float x, float y, Color color)
+        {
+            this.text = text;
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
         public MainMenu(string text, uint size, float x, float y)
         {
             this.text = text;
             this.size = size;
             this.x = x;
             this.y = y;
+        }
+        public MainMenu(string text, uint size, float x, float y, Color color)
+        {
+            this.text = text;
+            this.size = size;
+            this.x = x;
+            this.y = y;
+            this.color = color;
         }
     }
 }
--- SkySurfer/Assets/Scripts/Menu/MainMenuGameState.cs
+++ SkySurfer/Assets/Scripts/Menu/MainMenuGameState.cs
@@ -1,5 +1,6 @@
 ﻿using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 using SkySurfer.Assets.Scripts.Menu.Class;
 using System;
 using System.Collections.Generic;
@@ -11,28 +12,29 @@ namespace SkySurfer.Assets.Scripts.Menu
 {
     class MainMenuGameState : GameBaseState
     {
+        private int selected = 0;
+        private Color selectedColor = Color.Green;
         private Font font = new("../../../Assets/Fonts/Balbek-Personal.otf");
-        public override void Cleanup()
-        {
-
-        }
-
+        private MainMenu[] menu =
+            {
+                new(text: "Play", x: 0.15f, y: 0.10f),
+                new(text: "Settings", x: 0.15f, y: 0.25f),
+                new(text: "Credits", x: 0.15f, y: 0.40f),
+                new(text: "Exit", x: 0.15f, y: 0.80f),
+            };
         public override void Draw(RenderWindow window)
         {
             float windowX = window.Size.X;
             float windowY = window.Size.Y;
-            MainMenu[] menu =
-            {
-                new("Play", 50, windowX * 0.15f, windowY  * 0.10f),
-                new("Settings", 50, windowX * 0.15f, windowY * 0.25f),
-                new("Credits", 50, windowX * 0.15f, windowY * 0.40f),
-                new("Exit", 50, windowX * 0.15f, windowY * 0.80f),
-            };
 
-            foreach (MainMenu info in menu)
+            for (int i = 0; i < menu.Length; i++)
             {
-                TextPrint(window, info);
+                TextPrint(window, menu[i], i, windowX, windowY);
             }
+        }
+        public override void Cleanup()
+        {
+
         }
 
         public override void Exit()
@@ -54,10 +56,17 @@ namespace SkySurfer.Assets.Scripts.Menu
 
         }
 
-        private void TextPrint(RenderWindow window, MainMenu info)
+        private void TextPrint(RenderWindow window, MainMenu info, int i, float windowX, float windowY)
         {
             Text windowText = new(info.text, font, info.size);
-            windowText.Position = new(info.x, info.y);
+            windowText.Position = new(info.x * windowX, info.y * windowY);
+            if (selected == i)
+            {
+                windowText.FillColor = selectedColor;
+            } else
+            {
+                windowText.FillColor = info.color;
+            }
             window.Draw(windowText);
         }
     }
