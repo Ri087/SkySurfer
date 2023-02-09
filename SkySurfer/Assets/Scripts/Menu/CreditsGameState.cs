73ac28d1c504b08fe849b80f3de76ebfd0ed3578
JDURA <jeremy.dura@ynov.com>
2023-02-09 09:22:05 +0100

    [files] adding other state

---
+++ SkySurfer/Assets/Scripts/Menu/CreditsGameState.cs
@@ -0,0 +1,42 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySurfer.Assets.Scripts.Menu
+{
+    class CreditGameState : GameBaseState
+    {
+        public override void Cleanup()
+        {
+
+        }
+
+        public override void Draw(RenderWindow window)
+        {
+
+        }
+
+        public override void Exit()
+        {
+
+        }
+
+        public override void HandleInput()
+        {
+
+        }
+
+        public override void Init()
+        {
+            Console.WriteLine("Credits  init");
+        }
+
+        public override void Update(float deltaTime)
+        {
+
+        }
+    }
+}
---
+++ SkySurfer/Assets/Scripts/Menu/PauseMenuGameState.cs
@@ -0,0 +1,42 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkySurfer.Assets.Scripts.Menu
+{
+    internal class PauseMenuGameState : GameBaseState
+    {
+        public override void Cleanup()
+        {
+            //   throw new NotImplementedException();
+        }
+
+        public override void Draw(RenderWindow window)
+        {
+            //    throw new NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            //    throw new NotImplementedException();
+        }
+
+        public override void HandleInput()
+        {
+            //    throw new NotImplementedException();
+        }
+
+        public override void Init()
+        {
+            //    throw new NotImplementedException();
+        }
+
+        public override void Update(float deltaTime)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
