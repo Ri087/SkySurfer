d554c91c073804486a5a2007aae358e1cb8cc147
Bvernet <benjamin.vernet@ynov.com>
2023-02-09 12:10:26 +0100

    [add] delta time added

--- SkySurfer/Program.cs
+++ SkySurfer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SFML.Graphics;
+using SFML.System;
 using SFML.Window;
 using SkySurfer.Assets.Scripts;
 
@@ -10,21 +11,28 @@ namespace SkyRunner
         {
             //RenderWindow window = new RenderWindow(VideoMode.DesktopMode, "Sky Surfer", Styles.None);
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Sky Surfer", Styles.None);
-
             window.SetFramerateLimit(60);
+
+            Clock clock = new Clock();
+            float deltaTime = clock.Restart().AsSeconds();
 
             GameStateManager.GetInstance().GetStates().Peek().Init();
 
             while (window.IsOpen)
             {
+
                 window.DispatchEvents();
 
                 window.Clear();
 
-                GameStateManager.GetInstance().GetStates().Peek().Update(0f);
+
+                Console.WriteLine(deltaTime);
+                GameStateManager.GetInstance().GetStates().Peek().Update(deltaTime);
                 GameStateManager.GetInstance().GetStates().Peek().Draw(window);
 
                 window.Display();
+
+                deltaTime = clock.Restart().AsSeconds();
 
                 // Ferme la fenêtre
                 // window.Close();
